97dfc7b5fe32397b6c3b739c9388e5621ab6c3bb
DmytroChernii <131899183+DmytroChernii@users.noreply.github.com>
2026-04-17 08:04:55 +0300

    Update main.tf

--- task1/main.tf
+++ task1/main.tf
@@ -25,7 +25,7 @@ variable "do_token" {
 
 variable "prefix" {
   type    = string
-  default = "chernii-0704"
+  default = "chernii"
 }
 
 variable "region" {
@@ -34,7 +34,7 @@ variable "region" {
 }
 
 resource "digitalocean_vpc" "main" {
-  name     = "${var.prefix}-network"
+  name     = "chernii-vpc"
   region   = var.region
   ip_range = "10.10.10.0/24"
 }
@@ -58,26 +58,31 @@ resource "digitalocean_firewall" "main" {
     port_range       = "22"
     source_addresses = ["0.0.0.0/0"]
   }
+
   inbound_rule {
     protocol         = "tcp"
     port_range       = "80"
     source_addresses = ["0.0.0.0/0"]
   }
+
   inbound_rule {
     protocol         = "tcp"
     port_range       = "443"
     source_addresses = ["0.0.0.0/0"]
   }
+
   inbound_rule {
     protocol         = "tcp"
     port_range       = "8000-8003"
     source_addresses = ["0.0.0.0/0"]
   }
+
   outbound_rule {
     protocol              = "tcp"
     port_range            = "1-65535"
     destination_addresses = ["0.0.0.0/0"]
   }
+
   outbound_rule {
     protocol              = "udp"
     port_range            = "1-65535"
